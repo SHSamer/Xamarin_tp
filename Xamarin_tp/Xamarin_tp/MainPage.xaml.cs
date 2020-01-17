fce459779437622fa546f47779d81b2a0cd703fe
SHSamer <45460861+SHSamer@users.noreply.github.com>
2020-01-17 16:32:35 +0100

    Commit

---
+++ Xamarin_tp/Xamarin_tp/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin_tp.Models;
+
+namespace Xamarin_tp
+{
+    public partial class MainPage : ContentPage
+    {
+        private readonly HttpClient _client = new HttpClient();
+        private const string Url = "https://hmin309-embedded-systems.herokuapp.com/message-exchange/messages/";
+        private ObservableCollection<Account> account;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        async override protected void OnAppearing()
+        {
+            string responsecontent = await _client.GetStringAsync(Url);
+            List<Account> mylist = JsonConvert.DeserializeObject<List<Account>>(responsecontent);
+            account = new ObservableCollection<Account>(mylist);
+            ItemlistView.ItemsSource = account;
+            base.OnAppearing(); 
+        }
+        private async void Button_clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Cartepage());
+        }
+    }
+}
---
+++ Xamarin_tp/Xamarin_tp/Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin_tp.Models
+{
+    public class Account
+    {
+        public int id { get; set; }
+        public int student_id { get; set; }
+        public double gps_lat { get; set; }
+        public double gps_long { get; set; }
+        public string student_message { get; set; }
+    }
+}
